9fa1c6644d60d65814e3f7a267314447e4fbb61e
sukiram45 <sukiram2004@gmail.com>
2026-02-07 13:49:03 +0530

    Add files via upload
    assignment

---
+++ week1/EmployeeDetails.java
@@ -0,0 +1,25 @@
+package week1;
+public class EmployeeDetails {
+public static void main(String[] args) {
+		String name="JohnDoe";
+		int age=30;
+		double salary=55000.75;
+        char gender='M';
+        boolean acceptTerms=true;
+        float rating=4.5f;
+        long mobileNumber=9876543210l;
+        int pinCode=56001;
+        int luckyNumber=7;
+        
+        System.out.println("name="+name);
+        System.out.println("age="+age);
+        System.out.println("salary="+salary);
+        System.out.println("gender="+gender);
+        System.out.println("acceptTerms="+acceptTerms);
+        System.out.println("rating="+rating);
+        System.out.println("mobileNumber="+mobileNumber);
+        System.out.println("pinCode="+pinCode);
+        System.out.println("luckyNumber="+luckyNumber);
+	}
+
+}
---
+++ week1/Firefox.java
@@ -0,0 +1,17 @@
+package week1;
+public class Firefox {
+public static void main(String[] args) {
+	double browserVersion=100.2;
+	String browserName="firefox";
+	boolean isVisible=true;
+	int releaseYear=1998;
+	char browserLogo='f';
+	System.out.println("browserVersion="+browserVersion);
+	System.out.println("browserName="+browserName);
+	System.out.println("isVisible="+isVisible);
+	System.out.println("releaseYear="+releaseYear);
+	System.out.println("browserLogo="+browserLogo);
+	
+}
+
+}
